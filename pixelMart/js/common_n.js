3fe0ba88671e582b8b2506f0e025c3b6254254a5
rachok <vityarachok@gmail.com>
2016-05-30 13:10:36 +0300

    add responsive 8.html

---
+++ pixelMart/js/common_n.js
@@ -0,0 +1,83 @@
+$(document).ready(function () {
+
+	// Init home page carousel
+  var homeOwl = $("#home-carousel");
+  homeOwl.owlCarousel({
+    items: 1,
+    nav: true,
+    navText: "",
+    animateOut: 'fadeOut',
+    animateIn: 'fadeIn',
+    //autoplaySpeed: 5000,
+    //autoplayTimeout: 1000,
+    autoplay: true,
+    autoplayHoverPause: true,
+    mouseDrag: false,
+    loop: true,
+  });
+
+  // Config home page slider
+  function configSliderControls (elem) {
+	  $(elem).on('click', function (e) {
+	  	$(elem).removeClass('active');
+	  	$(this).addClass('active');
+	  	var dataToggle = $(this).data('toggle');
+	  	$("#home-carousel .owl-dot")[dataToggle].click();
+	  	e.preventDefault();
+	  })
+
+	  homeOwl.on('changed.owl.carousel', function(e) {
+	 		var activeNumber = null;
+	    $("#home-carousel .owl-dot").each(function (index) {
+	    	if($(this).hasClass('active')){
+	    		activeNumber = index;
+	    	}
+	    })
+			$(elem)[activeNumber].click();
+			e.preventDefault();
+	  })
+  }
+
+	configSliderControls('.carousel-toggle');
+	configSliderControls('.toggle-top-items');
+
+
+	// Module photo page: carousel init
+  var modOwl = $(".mod-carousel");
+  modOwl.owlCarousel({
+    items: 9,
+    margin: 5,
+    nav: true,
+    navText: "",
+    autoplayHoverPause: true,
+    loop: true,
+  });
+
+  // carousel toggle active class
+  function toggleActiveItem(elem) {
+  	if($(elem).length) {
+  		$(elem).click(function () {
+  			$(elem).removeClass('active');
+  			$(this).addClass('active');
+  		})
+  	}
+  }
+
+  toggleActiveItem('.mod-carousel-item');
+
+
+
+	// function addTooltip(elem, txt) {
+	//     if (elem) {
+	//         elem.append($('<div class="more-info"><div class="info-tooltip">' + txt + '</div></div>'));
+	//     }
+	// };
+
+	// addTooltip($('.best-label-tooltip'), 'Can choose more than one');
+
+
+
+
+
+
+})
